23a463d80f9a1e353b3f158c7248c9e95ac96976
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-08-22 00:00:12 +0700

    Bump AkkaTestKitVersion to 1.6.0

--- src/Akka.TestKit.NUnit.Tests/AssertionsTests.cs
+++ src/Akka.TestKit.NUnit.Tests/AssertionsTests.cs
@@ -5,72 +5,194 @@
 // </copyright>
 //----------------------------------------------------------------------
 
+using System;
+using System.Threading.Tasks;
+using Akka.Configuration;
 using NUnit.Framework;
 
-namespace Akka.TestKit.NUnit.Tests
+namespace Akka.TestKit.NUnit.Tests;
+
+[Parallelizable(ParallelScope.All)]
+public class AssertionsTests : TestKit
 {
-    [Parallelizable(ParallelScope.All)]
-    public class AssertionsTests : TestKit
+    private readonly NUnitAssertions _assertions;
+
+    public AssertionsTests()
     {
-        private readonly NUnitAssertions _assertions;
+        _assertions = new NUnitAssertions();
+    }
 
-        public AssertionsTests()
-        {
-            _assertions = new NUnitAssertions();
-        }
+    [Test]
+    public void Fail_should_throw()
+    {
+        Assert.Throws<AssertionException>(() => _assertions.Fail());
+    }
 
-        [Test]
-        public void Fail_should_throw()
-        {
-            Assert.Throws<AssertionException>(() => _assertions.Fail());
-        }
+    [Test]
+    public void AssertTrue_should_throw_on_false()
+    {
+        Assert.Throws<AssertionException>(() => _assertions.AssertTrue(false));
+    }
 
-        [Test]
-        public void AssertTrue_should_throw_on_false()
-        {
-            Assert.Throws<AssertionException>(() => _assertions.AssertTrue(false));
-        }
+    [Test]
+    public void AssertTrue_should_succeed_on_true()
+    {
+        _assertions.AssertTrue(true);
+    }
 
-        [Test]
-        public void AssertTrue_should_succeed_on_true()
-        {
-            _assertions.AssertTrue(true);
-        }
+    [Test]
+    public void AssertFalse_should_throw_on_true()
+    {
+        Assert.Throws<AssertionException>(() => _assertions.AssertFalse(true));
+    }
 
-        [Test]
-        public void AssertFalse_should_throw_on_true()
-        {
-            Assert.Throws<AssertionException>(() => _assertions.AssertFalse(true));
-        }
+    [Test]
+    public void AssertFalse_should_succeed_on_false()
+    {
+        _assertions.AssertFalse(false);
+    }
 
-        [Test]
-        public void AssertFalse_should_succeed_on_false()
-        {
-            _assertions.AssertFalse(false);
-        }
+    [Test]
+    public void AssertEqual_should_throw_on_not_equal()
+    {
+        Assert.Throws<AssertionException>(() => _assertions.AssertEqual(42, 4711));
+    }
 
-        [Test]
-        public void AssertEqual_should_throw_on_not_equal()
-        {
-            Assert.Throws<AssertionException>(() => _assertions.AssertEqual(42, 4711));
-        }
+    [Test]
+    public void AssertEqual_should_succeed_on_equal()
+    {
+        _assertions.AssertEqual(42, 42);
+    }
 
-        [Test]
-        public void AssertEqual_should_succeed_on_equal()
-        {
-            _assertions.AssertEqual(42, 42);
-        }
+    [Test]
+    public void AssertEqualWithComparer_should_throw_on_not_equal()
+    {
+        Assert.Throws<AssertionException>(() => _assertions.AssertEqual(42, 1142, Comparer));
+    }
 
-        [Test]
-        public void AssertEqualWithComparer_should_throw_on_not_equal()
-        {
-            Assert.Throws<AssertionException>(() => _assertions.AssertEqual(42, 42, (x, y) => false));
-        }
+    [Test]
+    public void AssertEqualWithComparer_should_succeed_on_equal()
+    {
+        Assert.DoesNotThrow(() => _assertions.AssertEqual(42, 42, Comparer));
+    }
 
-        [Test]
-        public void AssertEqualWithComparer_should_succeed_on_equal()
+    private static bool Comparer(int x, int y) => x == y;
+    
+    [TestCaseSource(nameof(_exceptionData))]
+    public void AssertThrows_should_succeed_on_any_thrown_exception(Exception exception)
+    {
+        Assert.That(_assertions.AssertThrows(ThisThrows), Is.EqualTo(exception));
+        return;
+
+        void ThisThrows()
         {
-            _assertions.AssertEqual(42, 4711, (x, y) => true);
+            throw exception;
         }
     }
+
+    [TestCaseSource(nameof(_exceptionData))]
+    public async Task AssertThrows_should_succeed_on_any_thrown_exception_async(Exception exception)
+    {
+        await Assert.ThatAsync(() => _assertions.AssertThrowsAsync(ThisThrows), Is.EqualTo(exception));
+        return;
+
+        async Task ThisThrows()
+        {
+            await Task.Yield();
+            throw exception;
+        }
+    }
+
+    [Test]
+    public void AssertThrows_should_fail_when_no_exception_was_thrown()
+    {
+        Assert.That(() => _assertions.AssertThrows(ThisDoesNotThrows), Throws.TypeOf(typeof(AssertionException)));
+        return;
+
+        void ThisDoesNotThrows()
+        {
+        }
+    }
+
+    [Test]
+    public async Task AssertThrows_should_fail_when_no_exception_was_thrown_async()
+    {
+        await Assert.ThatAsync(() => _assertions.AssertThrowsAsync(ThisDoesNotThrows), Throws.TypeOf(typeof(AssertionException)));
+        return;
+
+        async Task ThisDoesNotThrows()
+        {
+            await Task.Yield();
+        }
+    }
+
+    [TestCaseSource(nameof(_exceptionData))]
+    public void Generic_AssertThrows_should_succeed_only_on_specific_exception(Exception exception)
+    {
+        if (exception is ConfigurationException ex)
+        {
+            Assert.That(_assertions.AssertThrows<ConfigurationException>(ThisThrows), Is.EqualTo(ex));
+        }
+        else
+        {
+            Assert.Throws<AssertionException>(() => _assertions.AssertThrows<ConfigurationException>(ThisThrows));
+        }
+        return;
+
+        void ThisThrows()
+        {
+            throw exception;
+        }
+    }
+
+    [TestCaseSource(nameof(_exceptionData))]
+    public async Task Generic_AssertThrows_should_succeed_only_on_specific_exception_async(Exception exception)
+    {
+        if (exception is ConfigurationException ex)
+        {
+            await Assert.ThatAsync(() => _assertions.AssertThrowsAsync<ConfigurationException>(ThisThrows), Is.EqualTo(ex));
+        }
+        else
+        {
+            Assert.ThrowsAsync<AssertionException>(async () => await _assertions.AssertThrowsAsync<ConfigurationException>(ThisThrows));
+        }
+        return;
+
+        async Task ThisThrows()
+        {
+            await Task.Yield();
+            throw exception;
+        }
+    }
+
+    [Test]
+    public void Generic_AssertThrows_should_fail_when_no_exception_was_thrown()
+    {
+        Assert.That(() => _assertions.AssertThrows<ConfigurationException>(ThisDoesNotThrows), Throws.TypeOf(typeof(AssertionException)));
+        return;
+
+        void ThisDoesNotThrows()
+        {
+        }
+    }
+
+    [Test]
+    public async Task Generic_AssertThrows_should_fail_when_no_exception_was_thrown_async()
+    {
+        await Assert.ThatAsync(() => _assertions.AssertThrowsAsync<ConfigurationException>(ThisDoesNotThrows), Throws.TypeOf(typeof(AssertionException)));
+        return;
+
+        async Task ThisDoesNotThrows()
+        {
+            await Task.Yield();
+        }
+    }
+
+    private static object[] _exceptionData =
+    {
+        new object[] {new Exception("Wheee")},
+        new object[] {new ConfigurationException("Wheee")},
+        new object[] {new TimeoutException("Wheee")},
+        new object[] {new OperationCanceledException("Wheee")},
+    };
 }
--- src/Akka.TestKit.NUnit/NUnitAssertions.cs
+++ src/Akka.TestKit.NUnit/NUnitAssertions.cs
@@ -6,39 +6,81 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
-namespace Akka.TestKit.NUnit
+namespace Akka.TestKit.NUnit;
+
+/// <summary>
+/// Assertions for NUnit
+/// </summary>
+public class NUnitAssertions  : ITestKitAssertions
 {
-    /// <summary>
-    /// Assertions for NUnit
-    /// </summary>
-    public class NUnitAssertions  : ITestKitAssertions
-    {
         
-        public void Fail(string format = "", params object[] args)
-        {
-            Assert.Fail(string.Format(format, args));
-        }
+    public void Fail(string format = "", params object[] args)
+    {
+        Assert.Fail(string.Format(format, args));
+    }
 
-        public void AssertTrue(bool condition, string format = "", params object[] args)
-        {
-            Assert.That(condition, Is.True, string.Format(format, args));
-        }
+    public void AssertTrue(bool condition, string format = "", params object[] args)
+    {
+        Assert.That(condition, Is.True, string.Format(format, args));
+    }
 
-        public void AssertFalse(bool condition, string format = "", params object[] args)
-        {
-            Assert.That(condition, Is.False, string.Format(format, args));
-        }
+    public void AssertFalse(bool condition, string format = "", params object[] args)
+    {
+        Assert.That(condition, Is.False, string.Format(format, args));
+    }
 
-        public void AssertEqual<T>(T expected, T actual, string format = "", params object[] args)
-        {
-            Assert.That(actual, Is.EqualTo(expected), string.Format(format, args));
-        }
+    public void AssertEqual<T>(T expected, T actual, string format = "", params object[] args)
+    {
+        Assert.That(actual, Is.EqualTo(expected), string.Format(format, args));
+    }
 
-        public void AssertEqual<T>(T expected, T actual, Func<T, T, bool> comparer, string format = "", params object[] args)
+    public void AssertEqual<T>(T expected, T actual, Func<T, T, bool> comparer, string format = "", params object[] args)
+    {
+        Assert.That(actual, Is.EqualTo(expected).Using<T>(comparer), string.Format(format, args));
+    }
+
+    public Exception AssertThrows(Action action)
+    {
+        return Assert.Catch(() => action());
+    }
+
+    public TException AssertThrows<TException>(Action action) where TException : Exception
+    {
+        return Assert.Throws<TException>(() => action());
+    }
+
+    // No Nunit built-in solution for these, we have to do this manually
+    public async Task<Exception> AssertThrowsAsync(Func<Task> action)
+    {
+        try
         {
-            Assert.That(actual, Is.EqualTo(expected).Using<T>(comparer), string.Format(format, args));
+            await action();
         }
+        catch (Exception ex)
+        {
+            return ex;
+        }
+        throw new AssertionException("Expected an exception to be thrown but none was thrown.");
+    }
+
+    // No Nunit built-in solution for these, we have to do this manually
+    public async Task<TException> AssertThrowsAsync<TException>(Func<Task> action) where TException : Exception
+    {
+        try
+        {
+            await action();
+        }
+        catch (TException ex)
+        {
+            return ex;
+        }
+        catch (Exception ex)
+        {
+            throw new AssertionException($"Expected {typeof(TException).Name} to be thrown, but {ex.GetType()} was thrown instead.", ex);
+        }
+        throw new AssertionException($"Expected {typeof(TException).Name} to be thrown but none was thrown instead.");
     }
 }
